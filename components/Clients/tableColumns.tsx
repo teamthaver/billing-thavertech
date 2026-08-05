51c76bc956afe501a4049f163ab85ceb5fe1570a
Deependra Chauhan <deep.next@thavertech.com>
2026-08-05 12:39:57 +0530

    Done

--- components/Clients/tableColumns.tsx
+++ components/Clients/tableColumns.tsx
@@ -13,15 +13,27 @@ export const columns: ColumnDef<ClientData>[] = [
     accessorKey: "email",
     header: "Email",
     cell: ({ row }) => (
-      <span className="">{row.getValue("email")}</span>
+      <span className="">{row.getValue("email") ? row.getValue("email") : "NA"}</span>
     ),
   },
   {
     accessorKey: "phone",
     header: "Phone",
+    cell: ({ row }) => (
+      <span className="">{row.getValue("phone") ? row.getValue("phone") : "NA"}</span>
+    ),
   },
   {
-    accessorFn: (row) => `${row.city}, ${row.state}`,
+    accessorFn: (row) => {
+      const city = row.city?.trim();
+      const state = row.state?.trim();
+
+      if (!city && !state) return "N/A";
+      if (!city) return state;
+      if (!state) return city;
+
+      return `${city}, ${state}`;
+    },
     id: "location",
     header: "Location",
   },
@@ -31,15 +43,18 @@ export const columns: ColumnDef<ClientData>[] = [
     accessorFn: (row) => row.gst_number || row.tax_number,
     cell: ({ row }) => (
       <p className="text-xs font-mono">
-        { row.original.gst_number && <span className="text-blue-700 font-semibold">{row.original.gst_number}</span> }
-        { row.original.tax_number && <span className="text-green-700 font-semibold">{row.original.tax_number}</span> }
-        { !row.original.gst_number && !row.original.tax_number && <span>-</span> }
+        {row.original.gst_number && <span className="text-blue-700 font-semibold">{row.original.gst_number}</span>}
+        {row.original.tax_number && <span className="text-green-700 font-semibold">{row.original.tax_number}</span>}
+        {!row.original.gst_number && !row.original.tax_number && <span>-</span>}
       </p>
     ),
   },
   {
     accessorKey: "assigned_person",
     header: "Assigned Person",
+    cell: ({ row }) => (
+      <span className="">{row.getValue("assigned_person") ? row.getValue("assigned_person") : "NA"}</span>
+    ),
   },
   {
     accessorKey: "id",
